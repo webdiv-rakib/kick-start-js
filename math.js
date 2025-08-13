dd80f2b72a9a78546b277ea67e6a8a457c28a4e9
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-13 19:15:48 +0600

    first commit

---
+++ math.js
@@ -0,0 +1,20 @@
+/**
+ * *****************************************
+ * Math Needed for Programming
+ * *****************************************
+ * 
+ * 1. Number: +ve, -ve, 0 [50, -9, 0]
+ * 2. Integer: 2, 88, [odd, even]: 1,2,3,4
+ * 3. Float: 2.5, 3.5
+ * 4. Operation: +, -, *, /
+ * 5. More operation: %, []
+ * 6. Percentage: 5%, 50 taka er 10% koto?
+ * 7. Sum: 14, 18, 16
+ * 8. Average: 14, 18, 16
+ * 9. Max/Min: 
+ * 10. Series: 
+ * 11. Area: Triangle
+ * 12. 
+ */
+
+console.log(20)
