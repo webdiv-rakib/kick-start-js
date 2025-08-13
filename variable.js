811f986d2ed19a1ed0260944e8b6b82d629da709
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-13 19:36:03 +0600

    variable

---
+++ variable.js
@@ -0,0 +1,20 @@
+/**
+ * VARIABLE ==> vary + able
+ * 
+ *      5 things to declare a variable
+ * 1. var
+ * 2. variable name
+ * 3. =
+ * 4. value
+ * 5. semicolon
+ */
+
+var weight = 38;
+var price = 38;
+var year = 2025;
+var money = 15000;
+money = 800;
+var age;
+
+console.log(price);
+console.log(money);
