a2913bcf3d401113725dccd5c60e9d6d1b4acedd
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-17 04:56:02 +0600

    if else condition

---
+++ if.js
@@ -0,0 +1,33 @@
+/**
+ * Tui jodi amake khawas taille ami ashbo
+ * 
+ * ------STRUCTURE---------
+ * if(condition){
+ * 
+ * }
+ * 
+ */
+
+if (11 < 10) {
+    // console.log("I have a smaller value");
+}
+else {
+    // console.log("I have greater number");
+}
+// -------------------------------
+var biriyaniPrice = 200;
+if (biriyaniPrice < 300) {
+    // console.log("Mama Give me 1 plate Biriyani");
+}
+else {
+    // console.log("Taka nai jaiga");
+}
+
+/**
+ * if(condition){
+ *      execute code if condition above is true
+ * }
+ * else{
+ *      execute code if condition above is false
+ * }
+ */
