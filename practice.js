caf0024fe5218d8b6acdb95703737f0a5afc6252
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-17 04:12:04 +0600

    practice task

---
+++ practice.js
@@ -0,0 +1,24 @@
+// Problem - 1
+var money = 1000;
+var orangePrice = 400;
+var applePrice = 300;
+var total = orangePrice + applePrice;
+var moneyLeft = money - total;
+// console.log(moneyLeft);
+
+// Problem - 2
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+var totalMarks = mathematics + biology + chemistry + physics + bangla;
+var avgMarks = totalMarks / 5;
+// console.log(avgMarks.toFixed(2));
+
+// Problem - 3
+var number = 119;
+var givenNumber = 5;
+var remainder = number % givenNumber;
+// console.log(remainder);
+
