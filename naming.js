5d48075b6e10164714dc7641ec5cc1b955ead262
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-16 20:51:34 +0600

    naming convention

---
+++ naming.js
@@ -0,0 +1,29 @@
+// 1. no keyword in variable name
+var false = 55;
+var var = 50;
+
+// 2. no space or gap in variable name
+var is happy = false;
+var ishappy = true;
+
+// 3. no quote
+var "address" = "kochu khet";
+
+// 4. can not start with a number, but number other than the first letter is allowed
+var 3money = 88;
+var money = 45;
+var money2 = 55;
+
+// 5. name is case sensitive
+address
+Address
+ADDRESS
+addRess
+ADDress
+
+// 6. how to write a long variable
+var my current home address = 'Anorkilla Bandorban';
+var mycurrenthomeaddress = 'Anorkilla Bandorban';
+var my_current_home_address = 'Anorkilla Bandorban'; //snake case
+var myCurrentHomeAddress = 'Anorkilla Bandorban'; //cable case
+var MyCurrentHomeAddress = 'Anorkilla Bandorban'; //pascal case
