c97ec1133bf33595b730c21b9f5bfd74e31eed4e
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-16 21:26:00 +0600

    parseInt, parseFloat, toFixed

---
+++ number.js
@@ -0,0 +1,17 @@
+var orangePrice = 320;
+var chocolate = 0.5;
+var applePrice = parseInt('20');
+var applePrice = parseFloat('22.5');
+
+// console.log(applePrice);
+// console.log(typeof orangePrice);
+// console.log(typeof applePrice);
+// console.log(orangePrice + applePrice);
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+// console.log(total.toFixed(1));
+
+const num = 50 / 0;
+console.log(num);
