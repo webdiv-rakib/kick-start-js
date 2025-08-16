cdada6a25c3ac302c18d1415514675261b907499
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-17 03:46:00 +0600

    short hand

---
+++ mixedop.js
@@ -0,0 +1,4 @@
+var first = "Mobarak";
+var second = "Tobarak";
+var fullName = first + second;
+console.log(fullName);
---
+++ shorthand.js
@@ -0,0 +1,12 @@
+var price = 35;
+price = price + 15;
+price += 10;
+// console.log(price);
+
+var fridge = 35000;
+fridge -= 1000;
+// console.log(fridge);
+
+var orangePrice = 30;
+orangePrice *= 3;
+// console.log(orangePrice);
