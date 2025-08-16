543949d9b564c6b50f683eee719e04851aa69698
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-16 20:41:00 +0600

    types of variable

---
+++ types.js
@@ -0,0 +1,19 @@
+// String type variable
+var name = "rakib";
+var address = "tangail";
+var country = 'Bangladesh';
+console.log(country);
+console.log(typeof country);
+
+// Boolean type variable
+var isRich = true;
+var passed = false;
+console.log(passed);
+console.log(typeof passed);
+
+// Number type variable
+var price = 555;
+console.log(price);
+console.log(typeof price);
+
+
