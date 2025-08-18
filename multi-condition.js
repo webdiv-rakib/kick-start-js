241d934765358b70cd8bc0f57e9703320fb224d6
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-19 03:34:42 +0600

    and,or,complex condition

--- multi-condition.js
+++ multi-condition.js
@@ -1,2 +1,32 @@
-console.log("Abu Nayim Mohammad Rakib");
-console.log("Tanisha Rahman");
+const salary = 25000;
+const isBCS = true;
+const height = 68;
+const hasCar = false;
+
+// -----------Use of AND && condition-------------
+if (salary > 20000 && height > 66) {
+    console.log("Shuuuuuuuuu patro");
+}
+else {
+    console.log("Onno patro khujo");
+}
+
+
+// ----------Use of OR || condition---------------
+if (salary >= 5000 || height > 50 || isBCS == true) {
+    console.log("Esho baba kobul");
+}
+else {
+    console.log("Vag tui mogbul");
+}
+
+// ----------Use of Complex condition-------------
+if ((salary > 25000 && hasCar == true) || isBCS == true) {
+    console.log("14gushti raji");
+}
+if ((salary > 30000 || hasCar == true) && isBCS == true) {
+    console.log("14gushti raji");
+}
+else {
+    console.log("Baba ashte paro");
+}
