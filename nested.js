bc36832f5ef006856ba2e2667bb7a4402e9f73b4
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-19 05:37:26 +0600

    nested if-else

---
+++ nested.js
@@ -0,0 +1,17 @@
+const money = 150;
+if (money > 300) {
+    console.log('Bro you are rich');
+}
+else {
+    if (money > 200) {
+        console.log('Tui gorib o na borolok o na');
+    }
+    else {
+        if (money > 100) {
+            console.log('Dost kola kha calcium bara');
+        }
+        else {
+            console.log('Bondhu tui gorib');
+        }
+    }
+}
