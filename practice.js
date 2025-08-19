10bb1d955edfea360272743c37f9c2f85a49e225
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-19 06:58:37 +0600

    practice for js conditional

---
+++ practice.js
@@ -0,0 +1,13 @@
+// Problem - 1
+/***
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+let price = 500;
+if (price <= 500) {
+    // console.log("You got free coke");
+}
+else {
+    // console.log("You have to pay 30Taka for coke");
+}
