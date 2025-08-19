123ce3d4a272e7aaaaeace1387140d3287fcd390
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-20 04:31:11 +0600

    all practive problem solved

--- practice.js
+++ practice.js
@@ -6,10 +6,10 @@ Free Drinks
 */
 let price = 500;
 if (price <= 500) {
-    // console.log("You got free coke");
+    console.log("You got free coke");
 }
 else {
-    // console.log("You have to pay 30Taka for coke");
+    console.log("You have to pay 30Taka for coke");
 }
 
 
@@ -34,19 +34,19 @@ Create a JavaScript program that calculates the Body Mass Index (BMI) and assign
 let weight = 60;
 let height = 1.75;
 const bmi = weight / (height * height);
-// console.log("Your BMI is:", bmi.toFixed(2));
+console.log("Your BMI is:", bmi.toFixed(2));
 if (bmi < 18.5) {
-    // console.log("You are underweight");
+    console.log("You are underweight");
 }
 else {
     if (bmi >= 18.5 && bmi <= 24.5) {
-        // console.log("You are normal");
+        console.log("You are normal");
     }
     else if (bmi >= 25 && bmi <= 29.9) {
-        // console.log("You are overweight");
+        console.log("You are overweight");
     }
     else {
-        // console.log("You are obese");
+        console.log("You are obese");
     }
 }
 
@@ -71,19 +71,19 @@ Create a simple JavaScript program that takes a student's score as input and ret
 ***/
 let mark = 69;
 if (mark >= 90 && mark <= 100) {
-    // console.log("Grade: A");
+    console.log("Grade: A");
 }
 else if (mark >= 80 && mark <= 89) {
-    // console.log("Grade: B");
+    console.log("Grade: B");
 }
 else if (mark >= 70 && mark <= 79) {
-    // console.log("Grade: C");
+    console.log("Grade: C");
 }
 else if (mark >= 60 && mark <= 69) {
-    // console.log("Grade: D");
+    console.log("Grade: D");
 }
 else {
-    // console.log("Grade: F");
+    console.log("Grade: F");
 }
 
 
@@ -106,19 +106,23 @@ use nested if-else-if-else
 let friendScore = 81;
 let myScore = 80;
 if (friendScore >= 80) {
-    // console.log("Go for lunch");
+    console.log("Go for lunch");
 }
 else {
     if (friendScore <= 80 && friendScore >= 60) {
-        // console.log("Good Luck next time!");
+        console.log("Good Luck next time!");
     }
     else if (friendScore <= 60 && friendScore >= 40) {
-        // console.log("Message unseen");
+        console.log("Message unseen");
     }
     else if (friendScore <= 40 && myScore <= 80) {
-        // console.log("Block friend");
+        console.log("Block friend");
     }
 }
+
+
+
+
 
 // Problem - 5
 /***
@@ -133,6 +137,20 @@ write a simple if-else.
 also, write it using ternary operator.
 
  */
+let num1 = 50;
+let num2 = 40;
+if (num1 > num2) {
+    result = num1 * 2
+    console.log(result);
+}
+else {
+    result = num1 + num2;
+    console.log(result);
+}
+
+let result = (num1 > num2) ? num1 * 2 : num1 + num2;
+console.log(result);
+
 
 
 
@@ -146,3 +164,22 @@ Ticket fare Calculator
     - Senior citizens (age >= 60) gets a 15% Discount
     - Otherwise Regular ticket fare 800 tk
 */
+let age = 40;
+let ticketPrice = 800;
+let isStudent = true;
+if (age < 10) {
+    console.log("Free for children");
+}
+else if (age > 11 && age <= 30 && isStudent === true) {
+    const discount = ticketPrice * 0.5;
+    const afterDiscount = ticketPrice - discount;
+    console.log("After 50% discount student have to pay:", afterDiscount);
+}
+else if (age >= 60) {
+    const discount = ticketPrice * 0.15;
+    const afterDiscount = ticketPrice - discount;
+    console.log("Senior citizens have to pay:", afterDiscount);
+}
+else {
+    console.log("Regular ticket fare 800 Taka");
+}
