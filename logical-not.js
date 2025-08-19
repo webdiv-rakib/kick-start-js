8887e65634ecefcdc5fcdda475cdbf8b3617270d
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-19 06:38:31 +0600

    logical not

---
+++ logical-not.js
@@ -0,0 +1,24 @@
+const isLeader = true;
+if (isLeader) {
+    console.log("Leader asche! Chair char");
+}
+else {
+    console.log("Vitore dhukte dibi na");
+}
+// ------------------------------------
+const isPassed = true;
+if (!isPassed) {
+    console.log('Toke biye diye dibo');
+}
+else {
+    console.log('Tumi amader noyon er moni');
+}
+
+// ------------------------------------
+const isPassed = true;
+if (!!isPassed) {
+    console.log('Toke biye diye dibo');
+}
+else {
+    console.log('Tumi amader noyon er moni');
+}
