6f084b1b79eaee286d27f605d071a8fa768f3a32
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-22 05:22:36 +0600

    isArray

---
+++ js-array/isArray.js
@@ -0,0 +1,16 @@
+const friends = ['toymur', 'raju', 'sabbir', 'rashed'];
+const numbers = [];
+const food = 'rosh o gol la';
+console.log(Array.isArray(friends));
+console.log(Array.isArray(numbers));
+console.log(Array.isArray(food));
+/**
+ * Those you may need to know
+ * 1. includes()
+ * 2. indexOf()
+ * 3. isArray()
+ * 4. join()
+ * 5. concat()
+ * 6. slice()
+ * 7. splice()
+ */
