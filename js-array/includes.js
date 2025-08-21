a222cddd0ee0b40b769c8c05b68a46470eb220ca
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-22 05:04:45 +0600

    includes

---
+++ js-array/includes.js
@@ -0,0 +1,8 @@
+const friends = ['toymur', 'raju', 'sabbir','rashed'];
+console.log(friends.includes('raju'));
+if (friends.includes('rashed')) {
+    console.log("Ore ami goa mari");
+}
+else {
+    console.log("Cholo kacci bhai te jai");
+}
