31a4c5a3d2181b2dea615f1cb2cfe1e1c474a725
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-22 04:39:13 +0600

    push and pop

---
+++ js-array/push-pop.js
@@ -0,0 +1,19 @@
+// push way
+const ages = [];
+const numbers = [11, 22, 33, 44, 55];
+console.log(numbers);
+numbers.push(66);
+numbers.push(77);
+numbers.push(88);
+numbers.push(99, 111, 122);
+console.log(numbers);
+
+// pop way
+const friends = ['toymur', 'raju', 'sabbir'];
+console.log(friends);
+const out1 = friends.pop();
+const out2 = friends.pop();
+console.log(friends);
+console.log(out1, out2);
+
+
