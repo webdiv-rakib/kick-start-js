d51fda87f1de482e4ff236ab6bc3ee6ce5375b4f
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-22 05:34:09 +0600

    practice task

---
+++ js-array/practice-task.js
@@ -0,0 +1,33 @@
+/**
+ * Declare an array
+ * 1. Declare an array with 5 elements containing fruits
+ * 2. console log the 3rd index element
+ * 3. change the value of the 2nd index element to jambura
+ * 4. console log the final array
+ */
+const fruits = ['Mango', 'Jack fruit', 'Apple', 'Grap', 'Licchi'];
+// console.log(fruits);
+// console.log(fruits[2]);
+fruits[2] = ('Jambura');
+// console.log(fruits);
+
+
+
+
+/**
+ * Add or remove elements
+ * 1. Declare an array of 3 tourist destinations
+ * 2. Add a new tourist destination to your tourist array
+ * 3. Add two more to your array
+ * 4. Remove the last tourist destination you have added
+ * 5. display the final array as output
+ */
+const destination = ['Rajshahi', 'Sylhet', 'Chittagong'];
+// console.log(destination);
+destination.push('Khulna');
+// console.log(destination);
+destination.push('Cox bazar', 'Bandorban');
+// console.log(destination);
+destination.pop();
+// console.log(destination);
+
