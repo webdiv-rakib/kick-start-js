c5c40edc2474767cf0738a3ab2a390b53eede467
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-22 19:07:21 +0600

    rage

---
+++ js-loop/range.js
@@ -0,0 +1,11 @@
+let number = 0;
+while (number < 10) { //focus on the condition
+    number += 1;
+    console.log(number);
+}
+
+let num = 50;
+while (num < 100) {
+    num++;
+    console.log(num);
+}
