7e04ab0b4cdd2323de38de298a209f39ca40cd76
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-22 18:28:21 +0600

    while loop

---
+++ js-loop/while.js
@@ -0,0 +1,12 @@
+const number = 7;
+if (number < 1) {
+    console.log('Choto ekta number paisi');
+}
+console.log('Something new is cooking');
+
+// -------While Loop--------------
+let num = 0; //loop variable
+while (num < 5) {
+    num = num + 1;
+    console.log('Looping',num);
+}
