c84492ab61e6ac9804626315f3f1f7d9597e5354
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-22 18:10:17 +0600

    for-of loop

---
+++ js-loop/for-of.js
@@ -0,0 +1,10 @@
+const numbers = [12, 98, 45, 63, 21, 72];
+// console.log(numbers);
+for (const num of numbers) {
+    // console.log(num);
+}
+
+const fruits = ['Mango', 'Apple', 'Banana', 'Watermelon'];
+for (const fruit of fruits) {
+    console.log(fruit);
+}
