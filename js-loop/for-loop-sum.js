9e7ba39e0ec3d88e0fabdf6bb7f1c3c1bf37ef3f
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-23 03:13:47 +0600

    for loop sum

---
+++ js-loop/for-loop-sum.js
@@ -0,0 +1,6 @@
+let sum = 0;
+for (let i = 11; i <= 20; i++) {
+    sum = sum + i;
+    console.log(i);
+}
+console.log('Sum of numbers from 11 to 20 is:', sum);
--- js-loop/odd-even-for-loop.js
+++ js-loop/odd-even-for-loop.js
@@ -1,11 +1,11 @@
 // for (let i = 1; i <= 10; i++) {
 //     console.log(i);
 // }
-
+// ----Even----------
 for (let i = 0; i <= 20; i = i + 2) {
     // console.log(i);
 }
-
+// ----Odd---------
 for (let i = 1; i < 20; i = i + 2) {
     console.log(i);
 }
