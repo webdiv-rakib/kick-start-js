2c84a4ad140d1adf86fa15d91e8a9abb1d7f4318
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-23 03:56:58 +0600

    use of break

---
+++ js-loop/break.js
@@ -0,0 +1,17 @@
+// Break using for loop
+for (let i = 0; i < 15; i++) {
+    // console.log(i);
+    if (i >= 10) {
+        break;
+    }
+}
+
+// Break using while loop
+let n = 50;
+while (n >= 25) {
+    if (n <= 30) {
+        break;
+    }
+    console.log(n);
+    n--;
+}
