d952f360e351c3e0876a1e8f797d0e36260c6a3f
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-23 03:18:24 +0600

    decremental for loop

---
+++ js-loop/decremental-for.js
@@ -0,0 +1,13 @@
+// Incremental for loop;
+for (let i = 0; i <= 10; i++) {
+    // console.log(i);
+}
+
+// Decremental for loop;
+for (let i = 10; i >= 0; i--) {
+    // console.log(i);
+}
+
+for (let i = 20; i >= 10; i--) {
+    console.log(i);
+}
