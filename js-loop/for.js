e955cd60c6440b628811b32df52bb941a314f39c
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-23 02:53:51 +0600

    for-loop

---
+++ js-loop/for.js
@@ -0,0 +1,14 @@
+// let num = 0;
+// while (num < 5) {
+//     // console.log(num);
+//     num++;
+// }
+// --------Structure------------
+/**
+ * for (first part; second part; third part) {
+    loop CSSLayerStatementRule;
+}
+ */
+for (let num = 0; num <= 15; num++) {
+    console.log(num);
+}
---
+++ js-loop/odd-even-for-loop.js
@@ -0,0 +1,11 @@
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+for (let i = 0; i <= 20; i = i + 2) {
+    // console.log(i);
+}
+
+for (let i = 1; i < 20; i = i + 2) {
+    console.log(i);
+}
