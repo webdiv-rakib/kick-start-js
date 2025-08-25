032098b0aa572d1c6439dba34fd47cb8cbc59e08
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-26 03:57:37 +0600

    while loop practice problem solved

--- js-loop/while-loop-practice.js
+++ js-loop/while-loop-practice.js
@@ -7,7 +7,7 @@
 /*programming hero*/
 let num = 1;
 while (num <= 60) {
-    console.log("I will invest at least 6 hrs every single day for next 60 days! ", num);
+    // console.log("I will invest at least 6 hrs every single day for next 60 days! ", num);
     num++;
 }
 
@@ -36,7 +36,7 @@ let num1 = 61;
 while (num1 <= 100) {
     // console.log(num1);
     if (num1 % 2 !== 0) {
-        console.log("Odd Numbers:", num1);
+        // console.log("Odd Numbers:", num1);
     }
     num1 += 1;
 }
@@ -44,7 +44,7 @@ while (num1 <= 100) {
 let num2 = 78;
 while (num2 <= 98) {
     if (num2 % 2 === 0) {
-        console.log("Even Numbers:", num2);
+        // console.log("Even Numbers:", num2);
     }
     num2++;
 }
@@ -74,19 +74,19 @@ while (num3 <= 131) {
     // console.log(num3);
     if (num3 % 2 !== 0) {
         sum = sum + num3;
-        console.log("Total:", sum);
+        // console.log("Total:", sum);
     }
     num3++;
 }
-console.log("Final total:", sum2);
+// console.log("Final total:", sum2);
 
 let num4 = 206;
 let sum2 = 0;
 while (num4 <= 311) {
-    console.log(num4);
+    // console.log(num4);
     if (num4 % 2 === 0) {
         sum2 = sum2 + num4;
-        console.log("Even Numbers Sum:", sum2);
+        // console.log("Even Numbers Sum:", sum2);
     }
     num4++;
 }
@@ -103,10 +103,11 @@ As Ersa is learning now, she wants to explore more and more. Tell Ersa to genera
 
 
 /*programming hero*/
-let num5 = 1;
-while (num5 <= 5) {
-    // console.log(num5);
-    num5++;
+let i = 1;
+while (i <= 10) {
+    multi = 5 * i;
+    // console.log(multi);
+    i++;
 }
 
 
@@ -120,3 +121,8 @@ Implement a countdown timer that counts down from 21 to 15.
 
 
 /*programming hero*/
+let timer = 21;
+while (timer >= 15) {
+    console.log(timer);
+    timer--;
+}
