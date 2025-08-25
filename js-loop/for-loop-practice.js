64d61ec58294f2f64fd2806134b8713df950401e
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-26 03:01:15 +0600

    for loop practice using FOR solved

--- js-loop/for-loop-practice.js
+++ js-loop/for-loop-practice.js
@@ -96,5 +96,20 @@ Generate a multiplication table for number 9
 for (let i = 1; i <= 10; i++) {
     // console.log(i);
     multi = 9 * i;
-    console.log(multi);
+    // console.log(multi);
+}
+
+
+
+
+/***
+
+Implement a countdown timer that counts down from 81 to 65.
+
+ */
+
+
+/*programming hero*/
+for (let i = 81; i >= 65; i--) {
+    console.log(i);
 }
