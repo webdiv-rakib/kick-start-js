260b93ba8a22342696d66679ba0ab25dc2896694
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-25 21:39:39 +0600

    problem 2 solve

--- js-loop/for-loop-practice.js
+++ js-loop/for-loop-practice.js
@@ -6,5 +6,40 @@
 
 /*programming hero*/
 for (let i = 1; i <= 60; i++) {
-    console.log("I will invest at least 6 hrs every single day for next 60 days");
+    // console.log("I will invest at least 6 hrs every single day for next 60 days");
+}
+
+
+
+
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+
+/*programming hero*/
+for (let i = 61; i <= 100; i++) {
+    // console.log(i);
+    if (i % 2 !== 0) {
+        // console.log(i);
+    }
+}
+
+for (let i = 78; i <= 98; i++) {
+    // console.log(i);
+    if (i % 2 === 0) {
+        // console.log(i);
+    }
 }
