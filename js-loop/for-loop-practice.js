dd4e337ad5dcbd17ed8388ccef85dbf5a798e542
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-26 02:53:14 +0600

    problem 3 solved

--- js-loop/for-loop-practice.js
+++ js-loop/for-loop-practice.js
@@ -43,3 +43,41 @@ for (let i = 78; i <= 98; i++) {
         // console.log(i);
     }
 }
+
+
+
+
+/***
+
+Subtask-1:
+
+Display sum of all the odd numbers from 91 to 129.
+
+ */
+/***
+
+Subtask-2:
+
+Display sum of all the even numbers from 51 to 85.
+
+ */
+
+/*programming hero*/
+let sum = 0;
+for (let i = 91; i <= 129; i++) {
+    console.log(i);
+    if (i % 2 !== 0) {
+        sum = sum + i;
+        console.log(sum);
+    }
+}
+// console.log("Sum of all the odd number:",sum);
+
+let sum1 = 0;
+for (let i = 51; i <= 85; i++) {
+    console.log(i);
+    if (i % 2 === 0) {
+        sum1 = sum1 + i;
+    }
+}
+console.log("Sum of all the even numbers:", sum1);
