950afc9adb54ed6741a96b22e68d17e2998d6599
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-26 03:35:56 +0600

    break practice problem solved

--- js-loop/break-practice.js
+++ js-loop/break-practice.js
@@ -1,3 +1,39 @@
-for (let a = 0; a < 10; a++) {
-    console.log("I love you");
+/* 
+
+Write a loop 1 to 200. Use break to exit the loop once you find 100.
+
+*/
+for (let i = 1; i <= 200; i++) {
+    console.log(i);
+    if (i >= 100) {
+        break;
+    }
+}
+
+
+
+
+/* 
+Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
+*/
+let i = 1;
+while (i <= 200) {
+    console.log(i);
+    if (i >= 100) {
+        break;
+    }
+    i++;
+}
+
+
+
+
+/* 
+Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
+*/
+for (let i = 2; i <= 100; i++) {
+    if (i * i <= 100) {
+        console.log("First square number is:", i);
+        break;
+    }
 }
