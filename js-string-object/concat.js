6847d0b9cc4b1ee6787baae59799ff98ff58f962
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-28 04:02:04 +0600

    concat,includes

---
+++ js-string-object/concat.js
@@ -0,0 +1,9 @@
+const first = 'Abu';
+const second = 'Nayim';
+const third = 'Mohammad';
+const fourth = 'Rakib';
+const fullName = first + ' ' + second + ' ' + third + ' ' + fourth;
+console.log(fullName);
+const fullName2 = first.concat(' ').concat(second);
+console.log(fullName2);
+console.log(third.includes('m'));
