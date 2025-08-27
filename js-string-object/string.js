94d6a2dfa2d0b4a13c1a44f49d25de059bd523c1
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-27 22:05:58 +0600

    string is immutable

--- js-string-object/string.js
+++ js-string-object/string.js
@@ -2,3 +2,11 @@ const country = 'Bangladesh';
 const division = "Rajshahi";
 const district = `Tangail`;
 const thana = new String("Delduar");
+
+const numbers = [11, 12, 15, 20, 25];
+console.log(numbers.length);
+
+// String is immutable ----> not changeables
+const capital = 'Dhaka';
+console.log(capital.length);
+console.log(capital[0]);
