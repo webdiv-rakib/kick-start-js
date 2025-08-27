4cc3fcb107a9ce6f3cfcc96e017b2b6e403032ae
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-27 22:18:17 +0600

    to lowercase and to uppercase

---
+++ js-string-object/case.js
@@ -0,0 +1,12 @@
+const school = 'RAJ UK Uttara Model School';
+console.log(school);
+console.log(school.toLocaleLowerCase());
+console.log(school.toUpperCase());
+const subject = 'Chemistry';
+const book = 'chemistry';
+if (subject === book) {
+    // console.log("I am reading book aibar porikkha fatai felmu");
+}
+else {
+    // console.log('Hudai hudai pristha ultai');
+}
