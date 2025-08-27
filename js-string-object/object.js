f28daaed5a3aa54e32fb2f401e382d5e1ad83302
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-28 05:02:33 +0600

    object properties and values

---
+++ js-string-object/object.js
@@ -0,0 +1,23 @@
+const age = 21;
+const school = 'bindubasini';
+const isPassed = true;
+const subject1 = ['bangla', 'english', 'physics'];
+
+// Object
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false
+}
+
+const subject = {
+    name: 'biology',
+    teacher: 'rasheda man',
+    examDate: '30 Feb',
+    chapter: ['first', 'second', 'third'],
+    exams: {
+        name: 'Final Exam',
+        marks: 100
+    }
+}
