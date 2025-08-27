2d8915ebe8ebc78dc64dde6eea81bb1f8369c6c8
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-27 22:26:01 +0600

    trim to remove white space in string

--- js-string-object/case.js
+++ js-string-object/case.js
@@ -1,12 +1,21 @@
 const school = 'RAJ UK Uttara Model School';
-console.log(school);
-console.log(school.toLocaleLowerCase());
-console.log(school.toUpperCase());
+// console.log(school);
+// console.log(school.toLocaleLowerCase());
+// console.log(school.toUpperCase());
 const subject = 'Chemistry';
 const book = 'chemistry';
-if (subject === book) {
+if (subject.toLocaleLowerCase() === book.toLocaleLowerCase()) {
     // console.log("I am reading book aibar porikkha fatai felmu");
 }
 else {
     // console.log('Hudai hudai pristha ultai');
+}
+
+const drink = 'water';
+const liquid = '  water  ';
+if (drink.trim() === liquid.trim()) {
+    console.log('Panir opor nam Jibon');
+}
+else {
+    console.log("Somudre pani ase khaite pari na");
 }
