c631d9eedcff943afc3b267c9103e146aa0cc59b
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-08-29 22:28:56 +0600

    property

---
+++ js-string-object/property.js
@@ -0,0 +1,19 @@
+const person = {
+    name: 'sodor uddin',
+    age: 25,
+    profession: 'developer',
+    salary: 25000,
+    married: true,
+}
+// console.log(person);
+
+// dot notation
+// console.log(person.name);
+const income = person.salary;
+// console.log(income);
+
+// bracket notation
+// third bracket diye access kora
+console.log(person['married']);
+const boyos = person['age'];
+console.log(boyos);
