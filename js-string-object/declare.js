67df1fd7909a4716ecc835014099d289eca3c823
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-09-02 01:18:13 +0600

    way of declaring an object

---
+++ js-string-object/declare.js
@@ -0,0 +1,11 @@
+const pen = {
+    brand: 'matador',
+    price: 10,
+    color: 'black'
+}
+// console.log(pen);
+const pencil = new Object();
+console.log(pencil);
+const rubber = Object.create({});
+console.log(rubber);
+
