38808594fefbb561625d1d798eb0bcb17283c15d
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-09-04 03:00:14 +0600

    problem solving

--- js-array-2/array-looping-task.js
+++ js-array-2/array-looping-task.js
@@ -9,7 +9,48 @@ const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
 // for of method: 
 const forOfColors = [];
 for (const color of colors) {
-    console.log(color);
+    // console.log(color);
     forOfColors.unshift(color);
 }
-console.log(forOfColors);
+// console.log(forOfColors);
+
+// reverse using for loop
+const rev_side = [];
+for (let i = 0; i < colors.length; i++) {
+    const color1 = colors[i];
+    rev_side.unshift(color1);
+}
+// console.log(rev_side);
+
+// reverse side using fo loop
+for (let i = colors.length; i > 0; i--) {
+    const rong = colors[i];
+    // console.log(rong);
+}
+
+
+
+
+// 2. Write a JavaScript code to get the even numbers from an array using any looping technique.
+const numbers = [12, 98, 5, 41, 23, 78, 46];
+for (const num of numbers) {
+    if (num % 2 === 0) {
+        // console.log(num);
+    }
+}
+
+
+
+
+// 3 . Use a for...of loop to concatenate all the elements of an array into a single string.
+var names = ['Tom', 'Tim', 'Tin', 'Tik'];
+let fullName = '';
+for (let name of names) {
+    fullName += name;
+}
+console.log(fullName);
+
+
+
+
+//4. const statement = 'I am a hard working person'
