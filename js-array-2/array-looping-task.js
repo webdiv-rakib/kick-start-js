5e1cb55c25a9f8c481002da01e2a830c2cd3687e
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-09-04 03:34:20 +0600

    array-looping task complete

--- js-array-2/array-looping-task.js
+++ js-array-2/array-looping-task.js
@@ -3,16 +3,16 @@
 const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
 
 // reverse method:
-// colors.reverse();
-// console.log(colors);
+colors.reverse();
+console.log(colors);
 
 // for of method: 
 const forOfColors = [];
 for (const color of colors) {
-    // console.log(color);
+    console.log(color);
     forOfColors.unshift(color);
 }
-// console.log(forOfColors);
+console.log(forOfColors);
 
 // reverse using for loop
 const rev_side = [];
@@ -20,12 +20,12 @@ for (let i = 0; i < colors.length; i++) {
     const color1 = colors[i];
     rev_side.unshift(color1);
 }
-// console.log(rev_side);
+console.log(rev_side);
 
 // reverse side using fo loop
 for (let i = colors.length; i > 0; i--) {
     const rong = colors[i];
-    // console.log(rong);
+    console.log(rong);
 }
 
 
@@ -35,7 +35,7 @@ for (let i = colors.length; i > 0; i--) {
 const numbers = [12, 98, 5, 41, 23, 78, 46];
 for (const num of numbers) {
     if (num % 2 === 0) {
-        // console.log(num);
+        console.log(num);
     }
 }
 
@@ -53,4 +53,41 @@ console.log(fullName);
 
 
 
-//4. const statement = 'I am a hard working person'
+//4. Reverse the words of a sentence. Only the position of the word will be reversed. check out the output
+const statement = 'I am a hard working person'
+const reversed = statement.split(' ').reverse().join(' ');
+console.log(reversed);
+
+
+
+
+//5. Copy the given array into another array so that changing the copy does not affect the original.Change the first element of the copied array to 99.
+const givenNumbers = [1, 2, 3];
+const copyNumbers = [...givenNumbers];
+givenNumbers.reverse();
+console.log(givenNumbers);
+console.log(copyNumbers);
+
+
+
+
+//6. Given an array of student objects, print each student’s name and marks.
+const studentObjects = [
+    { name: "John", marks: 85 },
+    { name: "Alice", marks: 90 }
+]
+for (const studentObject of studentObjects) {
+    console.log(studentObject);
+}
+
+
+
+
+// 7. Given a 2D array, update the value at second row first item to 99 and print the updated array.
+const matrix = [
+    [1, 2],
+    [3, 4],
+    [5, 6]
+]
+matrix[1][0] = 99;
+console.log(matrix);
