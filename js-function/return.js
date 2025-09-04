98c098ec290812012824cb7af18366703d00fec4
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-09-05 04:41:32 +0600

    return

---
+++ js-function/return.js
@@ -0,0 +1,14 @@
+function tenTimes(number) {
+    const result = number * 10;
+    return result;
+}
+
+function cutHalf(number) {
+    const half = number / 2;
+    return half;
+}
+
+tenTimes(5);
+
+const outPut = tenTimes(5);
+console.log('OutPut:', outPut);
