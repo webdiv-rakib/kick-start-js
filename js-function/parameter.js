337a6e92406476792d96796851824f911aefccfe
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-09-05 04:04:59 +0600

    parameter and multiple parameter

---
+++ js-function/parameter.js
@@ -0,0 +1,22 @@
+function vaatKhao() {
+    console.log('hat dhow');
+    console.log('boso');
+    console.log('plate e khabar dabar naw');
+    console.log('gopat gopat kore khaw');
+}
+// vaatKhao();
+
+function square(number) {
+    const borgo = number * number;
+    console.log('square of the given number is:', borgo);
+}
+// square(2);
+// square(5);
+// square(10);
+
+// -------Sum using Function----------
+function add(num1, num2) {
+    const sum = num1 + num2;
+    console.log('Sum of two number:', sum);
+}
+add(2, 3);
