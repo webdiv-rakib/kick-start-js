f3a26057d2a60a506003bae92a77e39c65e06e09
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-09-05 03:43:35 +0600

    function structure and function call

--- js-function/function.js
+++ js-function/function.js
@@ -1,1 +1,10 @@
 // Function
+
+// function declaration;
+const number = 45;
+function fanOffKor() {
+    console.log('Bosa theke uthe dara');
+    console.log('Walk towards the switch');
+    console.log('Click the switch to off of the fan');
+}
+fanOffKor();
