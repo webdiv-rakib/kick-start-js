8e8206a05147b7f912df54c95b0c3a1c3a10556e
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-09-06 04:32:05 +0600

    examples of array-function sum,sub,multi

--- js-function/array-function.js
+++ js-function/array-function.js
@@ -9,11 +9,38 @@
 function sumOfNumbers(numbers) {
     let sum = 0;
     for (const number of numbers) {
-        console.log(number);
+        // console.log(number);
         sum = sum + number;
     }
     return sum;
 }
 const numbs = [22, 33, 44, 55, 66, 77, 88, 99];
 const sum = sumOfNumbers(numbs);
-console.log("Sum of numbers is:", sum);
+// console.log("Sum of numbers is:", sum);
+
+// ==========Example-Subtract==========
+function subOfNumbers(numbers) {
+    let sub = 0;
+    // console.log(numbers);
+    for (const num of numbers) {
+        sub = sub - num;
+    }
+    return sub;
+}
+const numberOfArray = [10, -20, 20, -5, 15, 2];
+const array = subOfNumbers(numberOfArray);
+// console.log('Substract of array:', array);
+
+
+// =========Example-Multiplication==========
+function multiOfNumbers(number) {
+    console.log(number);
+    let multi = 1;
+    for (const num of number) {
+        multi = multi * num;
+    }
+    return multi;
+}
+const multi = [2, 2, 2, 2];
+const multiArray = multiOfNumbers(multi);
+console.log("Multi of numbers: ", multiArray);
