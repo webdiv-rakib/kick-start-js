d7083b496bdeed4f1fcf8bafd76370a366d87ac1
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-09-06 03:48:40 +0600

    array-function sum

--- js-function/array-function.js
+++ js-function/array-function.js
@@ -4,9 +4,15 @@
  * step-2: call check whether the function is called properly
  * step-3: set a parameter(s)
  * step-4: pass the parameter(s), check whether parameter is passed in a proper format
+ * step-5: do the function tasks (step by step)
  */
 function sumOfNumbers(numbers) {
-    console.log(numbers);
+    let sum = 0;
+    for (const number of numbers) {
+        console.log(number);
+        sum = sum + number;
+    }
+    return sum;
 }
 const numbs = [22, 33, 44, 55, 66, 77, 88, 99];
 const sum = sumOfNumbers(numbs);
