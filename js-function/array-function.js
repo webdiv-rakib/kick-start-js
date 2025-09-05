f814b2c856a400a9ef41ad3e9055f49685fb76c9
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-09-05 22:50:47 +0600

    step by step function check

---
+++ js-function/array-function.js
@@ -0,0 +1,13 @@
+/**
+ * Objective: write a function to give me the sum of all numbers in array
+ * step-1: declare a function
+ * step-2: call check whether the function is called properly
+ * step-3: set a parameter(s)
+ * step-4: pass the parameter(s), check whether parameter is passed in a proper format
+ */
+function sumOfNumbers(numbers) {
+    console.log(numbers);
+}
+const numbs = [22, 33, 44, 55, 66, 77, 88, 99];
+const sum = sumOfNumbers(numbs);
+console.log("Sum of numbers is:", sum);
