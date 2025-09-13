5896482ab5738c2401fd83be02bda1757a7fe612
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-09-13 21:11:23 +0600

    working on simple code solve

--- js-simple-coding-problem/index.js
+++ js-simple-coding-problem/index.js
@@ -1,4 +1,13 @@
-console.log("rakib");
-console.log('Just got my laptop');
-console.log("Abu Nayim Mohammad Rakib");
-console.log("Daffodil International University");
+//covert Inch to Feet
+function inchToFeet(inch) {
+    const feet = inch / 12;
+    return feet;
+}
+const result = inchToFeet(160);
+console.log(result)
+
+//Conver Feet to Inch
+function inchToFeet2(inch) {
+    console.log(inch);
+}
+inchToFeet2(100);
