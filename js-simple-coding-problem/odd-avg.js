d01cd8663aa733ef21ca6ed858cf19514e3d8180
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-09-17 03:25:14 +0600

    avg of even array

--- js-simple-coding-problem/odd-avg.js
+++ js-simple-coding-problem/odd-avg.js
@@ -23,8 +23,23 @@ const numbers = [2, 1, 5, 7, 8, 9, 12, 11, 13, 17, 19, 20, 22];
 const avg = oddAverage(numbers);
 // console.log('average of all odd numbers:', avg);
 // ===For even numbers========
-function evenAverage(numbers){
-    console.log(numbers);
+function evenAverage(numbers) {
+    const evens = [];
+    for (const num of numbers) {
+        if (num % 2 === 0) {
+            evens.push(num);
+
+        }
+    }
+    const evenLen = evens.length;
+    let evenSum = 0;
+    for (const even of evens) {
+        evenSum = evenSum + even;
+    }
+    const evenAvg = evenSum / evenLen;
+    return evenAvg;
+    // console.log(evens);
 }
-const nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 const evenAvg = evenAverage(nums);
+console.log('Avg of Even Numbers:', evenAvg);
