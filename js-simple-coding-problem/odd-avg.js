b1c857775ed352a8b44f77c81d1492ba1557497e
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-09-16 20:58:21 +0600

    even average

--- js-simple-coding-problem/odd-avg.js
+++ js-simple-coding-problem/odd-avg.js
@@ -21,4 +21,10 @@ function oddAverage(numbers) {
 }
 const numbers = [2, 1, 5, 7, 8, 9, 12, 11, 13, 17, 19, 20, 22];
 const avg = oddAverage(numbers);
-console.log('average of all odd numbers:', avg);
+// console.log('average of all odd numbers:', avg);
+// ===For even numbers========
+function evenAverage(numbers){
+    console.log(numbers);
+}
+const nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+const evenAvg = evenAverage(nums);
