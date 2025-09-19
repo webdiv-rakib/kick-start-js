e07b6f28b254b3f24fc94eb2c91e74ea5f0fa9af
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-09-19 22:05:12 +0600

    problem 2 solved

--- js-simple-coding-problem/problem-practice.js
+++ js-simple-coding-problem/problem-practice.js
@@ -4,4 +4,55 @@ function tempConversion(temp) {
     return result;
 }
 const fahrenheit = tempConversion(38);
-console.log('Temperature is fahrenheit:', fahrenheit);
+// console.log('Temperature is fahrenheit:', fahrenheit);
+
+
+
+
+// 2. You are given an array of numbers. Count how many times the a number is repeated in the array.
+
+// ===This one not working========
+// function numberOfElements(numbers) {
+//     const duplicate = [];
+//     for (const num of numbers) {
+//         if (duplicate.includes(num) !== false) {
+//             duplicate.push(num);
+//         }
+//     }
+//     const dupliLen = duplicate.length;
+//     console.log(duplicate);
+//     return dupliLen;
+
+// }
+// const numbers = numberOfElements([5, 6, 11, 12, 98, 5]);
+// console.log('Duplicate number count:', numbers);
+
+function numberOfElements(numbers, find) {
+    let count = 0;
+    for (const num of numbers) {
+        if (num === find) {
+            count++;
+        }
+    }
+    return count;
+}
+const find = 5;
+const numbers = [5, 6, 11, 12, 98, 5, 5];
+// console.log('Output:', numberOfElements(numbers, find));
+
+// ------Another problem---------
+function notExit(array, search) {
+    // console.log(array);
+    // console.log(search);
+    const howManyTimes = 0;
+    for (const newArray of array) {
+        if (newArray === search) {
+            howManyTimes++;
+        }
+    }
+    return howManyTimes;
+}
+const search = 25;
+const array = [5, 6, 11, 12, 15, 98, 5];
+const outPut = notExit(array, search);
+console.log("Output:", outPut);
