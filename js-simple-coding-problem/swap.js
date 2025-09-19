7a1732ae461f32072fcb399814c1727c67ce641a
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-09-19 21:22:54 +0600

    swap variable,with and without temp and destructing method

--- js-simple-coding-problem/swap.js
+++ js-simple-coding-problem/swap.js
@@ -4,6 +4,17 @@ let a = 5;
 let b = 10;
 console.log(a, b);
 
+// a = b;
+// b = a;
+const temp = a;
 a = b;
-b = a;
+b = temp;
 console.log(a, b);
+// ==========================
+let x = 10;
+let y = 20;
+console.log(x, y);
+[x, y] = [y, x];
+console.log(x, y);
+
+// =========================
