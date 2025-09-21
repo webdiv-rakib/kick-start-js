6f55908bceb9d938e0b8e8ecd0fcbf426cafb0db
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-09-21 23:05:07 +0600

    commit done

--- js-simple-coding-problem-2/max.js
+++ js-simple-coding-problem-2/max.js
@@ -1,8 +1,19 @@
-const disha  = 56;
-const ankit  = 45;
-if(disha>ankit){
+const disha = 56;
+const ankit = 45;
+if (disha > ankit) {
     console.log("Disha has more marks");
 }
-else{
+else {
     console.log("Ankit has more marks");
 }
+
+// inside a function
+function getMax(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    }
+    else {
+        return num2;
+    }
+}
+console.log(getMax(30, 20));
