f2b4d3c328b99ac61b622fef886dd8aae3b0e37a
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-09-24 17:51:39 +0600

    find out highest mark

--- js-simple-coding-problem-2/min.js
+++ js-simple-coding-problem-2/min.js
@@ -9,4 +9,20 @@ function getMin(numbers) {
     return min;
 }
 const cheap = getMin(prices);
-console.log('Cheapest one is:', cheap);
+// console.log('Cheapest one is:', cheap);
+
+// ============Highest==============
+const marks = [20, 50, 40, 30, 60, 70, 80, 90];
+function getMax(numbers) {
+    // console.log(numbers);
+    let max = numbers[0];
+    for (const mark of marks) {
+        console.log(mark);
+        if (mark > max) {
+            max = mark;
+        }
+    }
+    return max;
+}
+const highest = getMax(marks);
+console.log('Highest marks is:', highest);
