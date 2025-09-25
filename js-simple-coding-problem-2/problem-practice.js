f19bc354823a95a922bc770a68b4cc258633ad53
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-09-25 19:26:40 +0600

    problem 1 solved

--- js-simple-coding-problem-2/problem-practice.js
+++ js-simple-coding-problem-2/problem-practice.js
@@ -1,1 +1,16 @@
 // 1. Find the lowest number in the array below.
+const heights = [167, 190, 120, 165, 137];
+function lowestHeights(numbers) {
+    console.log(numbers);
+    let min = numbers[0];
+    for (const num of numbers) {
+        console.log(num);
+        if (min > num) {
+            min = num;
+        }
+    }
+    console.log(min);
+    return min;
+}
+const lowest = lowestHeights(heights);
+console.log('Lowest Height:', lowest);
