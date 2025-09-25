01e45e76d9158212757a74e3b5009d6af25e2819
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-09-25 19:37:51 +0600

    problem 2 solved

--- js-simple-coding-problem-2/problem-practice.js
+++ js-simple-coding-problem-2/problem-practice.js
@@ -1,16 +1,33 @@
 // 1. Find the lowest number in the array below.
 const heights = [167, 190, 120, 165, 137];
 function lowestHeights(numbers) {
-    console.log(numbers);
+    // console.log(numbers);
     let min = numbers[0];
     for (const num of numbers) {
-        console.log(num);
+        // console.log(num);
         if (min > num) {
             min = num;
         }
     }
-    console.log(min);
+    // console.log(min);
     return min;
 }
 const lowest = lowestHeights(heights);
-console.log('Lowest Height:', lowest);
+// console.log('Lowest Height:', lowest);
+
+
+
+
+// 2. Find the friend with the smallest name.
+const friends = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
+function smallestName(names) {
+    let small = names[0];
+    for (const friend of names) {
+        if (friend.length < small.length) {
+            small = friend;
+        }
+    }
+    return small;
+}
+const name = smallestName(friends);
+console.log('Smallest name is:', name);
